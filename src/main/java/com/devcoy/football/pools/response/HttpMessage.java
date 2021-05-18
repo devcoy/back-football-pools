9715cb576b94d3c1d1b410becc7b4f0c08e7b843
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-18 00:09:57 -0500

    implementación del paquete para modularizar las respuestas http

---
+++ src/main/java/com/devcoy/football/pools/response/HttpMessage.java
@@ -0,0 +1,22 @@
+package com.devcoy.football.pools.response;
+
+public interface HttpMessage {
+	
+	// CRUD
+		public static final String CREATED = "Se ha creado el registro correctamente.";
+		public static final String READED = "Se han obtenido los registros correctamente.";
+		public static final String UPDATED = "Se ha actualizado el registro correctamente.";
+		public static final String DELETED = "Se ha eliminado el registro correctamente.";
+
+		// 2xx
+		public static final String OK = "La solicitud se ha realizado correctamente.";
+		public static final String ACCEPTED = "Hemos recibido la solicitud, danos unos minutos en lo que la procesamos.";
+		public static final String NO_CONTENT = "Los registros que intentas obtener al parecer no existen.";
+
+		// 4xx
+		public static final String NOT_FOUND = "El contenido al que intentas acceder, al parecer no existe.";
+
+		// 5xx
+		public static final String DB_EXCEPTION = "Estamos experiementando fallas en la Base de Datos.";
+
+}
