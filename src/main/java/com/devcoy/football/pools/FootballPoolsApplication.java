3614c6e06cd5c63e421caf6c46274df1715a5263
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-17 23:28:34 -0500

    spring boot initializr

---
+++ src/main/java/com/devcoy/football/pools/FootballPoolsApplication.java
@@ -0,0 +1,13 @@
+package com.devcoy.football.pools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcoy/football/pools/FootballPoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoy.football.pools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
