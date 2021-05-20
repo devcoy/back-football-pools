17781af017518be778899ea96e9ada5ef4810e59
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-19 20:08:00 -0500

    implementación de la relación entre Championship y SoccerDay

--- src/main/java/com/devcoy/football/pools/model/Championship.java
+++ src/main/java/com/devcoy/football/pools/model/Championship.java
@@ -1,14 +1,19 @@
 package com.devcoy.football.pools.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -20,8 +25,12 @@ public class Championship implements Serializable {
 	private Long id;
 
 	@NotNull
-	@NotEmpty	
+	@NotEmpty
 	private String name;
+
+	@JsonIgnoreProperties(value = { "championship" }, allowSetters = true)
+	@OneToMany(mappedBy = "championship", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<SoccerDay> soccerDays;
 
 	public Long getId() {
 		return id;
@@ -37,6 +46,43 @@ public class Championship implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<SoccerDay> getSoccerDays() {
+		return soccerDays;
+	}
+
+	public void setSoccerDays(List<SoccerDay> soccerDays) {
+		this.soccerDays.clear();
+
+		/**
+		 * Por cada Joranda, debemos setear el Torneo
+		 */
+		this.soccerDays.forEach(soccerDay -> this.addSoccerDay(soccerDay));
+		this.soccerDays = soccerDays;
+	}
+
+	public void addSoccerDay(SoccerDay soccerDay) {
+		this.soccerDays.add(soccerDay);
+		soccerDay.setChampionship(this);
+	}
+
+	public void removeSoccerDay(SoccerDay soccerDay) {
+		this.soccerDays.remove(soccerDay);
+
+		soccerDay.setChampionship(null);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Championship)) {
+			return false;
+		}
+		Championship championshipCast = (Championship) obj;
+		return this.id != null && this.id.equals(championshipCast.getId());
 	}
 
 	/**
