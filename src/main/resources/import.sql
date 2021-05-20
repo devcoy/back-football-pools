907517c6ed9a0e0531920d847e2ba2ec3baca32c
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-19 20:55:19 -0500

    se creo el fichero para automimportar registro a la DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+// championships
+
+INSERT INTO championships(name) VALUES("AP-2019");
+INSERT INTO championships(name) VALUES("CL-2020");
+INSERT INTO championships(name) VALUES("AP-2020");
+INSERT INTO championships(name) VALUES("AP-2021");
+
