645de298c12efa8019ca16483b615ab6250f425c
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-28 18:34:09 -0500

    class de Spring security config

---
+++ src/main/java/com/devcoy/football/pools/auth/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.devcoy.football.pools.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsService userService;
+
+	// Método para encriptar la contraseña usando BCrypt
+	@Bean() // con @Bean registramos el Objeto que retorna un el método
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// Sobreescribímos para registrar en el Authentication Manager de Spring
+	// Security para authenticar
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
+	}
+
+}
