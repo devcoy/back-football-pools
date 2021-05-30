6c63da7fff480d867631b57f1fad288602985f8c
Jorge Cervantes <jorgecdev@gmail.com>
2021-05-29 19:37:12 -0500

    user service and user service impl

--- src/main/java/com/devcoy/football/pools/service/UserService.java
+++ src/main/java/com/devcoy/football/pools/service/UserService.java
@@ -1,43 +1,9 @@
 package com.devcoy.football.pools.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import com.devcoy.football.pools.model.User;
-import com.devcoy.football.pools.repo.UserRepo;
 
-@Service
-public class UserService implements UserDetailsService {
-
-	@Autowired
-	UserRepo userRepo;
-
-	@Override
-	@Transactional
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-		// Obtenemos el usuario de la DB a través de email
-		User user = this.userRepo.findByEmail(username);
-
-		// Obtenemos los Authorities (roles) que tenga asignados el usuario
-		List<GrantedAuthority> authorities = user.getRoles().stream()
-				.map(role -> new SimpleGrantedAuthority(role.getName()))
-				//.peek(authority -> logger.info("Role: " + authority.getAuthority()))
-				.collect(Collectors.toList());
-
-		// Creamos la instancia de User
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
-				user.getEnabled(), true, true, true, null);
-	}
+public interface UserService {
+	
+	public User findByEmail(String email);
 
 }
---
+++ src/main/java/com/devcoy/football/pools/service/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.devcoy.football.pools.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devcoy.football.pools.model.User;
+import com.devcoy.football.pools.repo.UserRepo;
+import org.slf4j.Logger;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+	private Logger logger = org.slf4j.LoggerFactory.getLogger(UserServiceImpl.class);
+
+	@Autowired
+	UserRepo userRepo;
+
+	@Override
+	@Transactional(readOnly = true)
+	public User findByEmail(String email) {
+		return userRepo.findByEmail(email);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// Obtenemos el usuario de la DB a través de email
+		User user = this.userRepo.findByEmail(username);
+
+		if (user == null) {
+			logger.error("Error al iniciar sesión, no existe el usuario " + username + " en el sistema");
+			throw new UsernameNotFoundException(
+					"Error al iniciar sesión, no existe el usuario " + username + " en el sistema");
+		}
+
+		// Obtenemos los Authorities (roles) que tenga asignados el usuario
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName()))
+				// .peek(authority -> logger.info("Role: " + authority.getAuthority()))
+				.collect(Collectors.toList());
+
+		// Creamos la instancia de User
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				user.getEnabled(), true, true, true, authorities);
+	}
+
+}
