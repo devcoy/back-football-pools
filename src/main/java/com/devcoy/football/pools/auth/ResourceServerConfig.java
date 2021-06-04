ef0b73403d9cc804e66bb1ceda415d13898c0cf4
Jorge Cervantes <jorgecdev@gmail.com>
2021-06-03 22:58:18 -0500

    CORS config

--- src/main/java/com/devcoy/football/pools/auth/ResourceServerConfig.java
+++ src/main/java/com/devcoy/football/pools/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.devcoy.football.pools.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -20,7 +29,47 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/club", "/api/championship/**").permitAll()
 				.antMatchers(HttpMethod.DELETE, "/api/championship/**").permitAll()
 				// cualquier otra ruta y método http necesitará autenticación
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+				// CORS config
+				.and().cors().configurationSource(corsConfigurationSource());
+	}
+
+	// CORS config
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+
+		// Dominios permitidos
+		corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+
+		// Métodos permitidos
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
+
+		corsConfig.setAllowCredentials(true);
+
+		// Definimos todas las cabeceras que vamos a enviar desde el Front
+		corsConfig.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+		// Registrar el configCors para todas los endpoints del backend
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return source;
+	}
+
+	/**
+	 * Registrar un filtro en la prioridad más alta de Spring Secure para que se
+	 * aplique a todo
+	 */
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(
+				new CorsFilter(corsConfigurationSource()));
+
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return bean;
 	}
 
 }
